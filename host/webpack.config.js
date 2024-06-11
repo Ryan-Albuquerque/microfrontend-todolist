57c427a3cad8ce5dbc605533c0bdeaef80d8bff5
Ryan Albuquerque <ryan.silva@minu.co>
2024-06-11 05:04:28 -0300

    webpack config

---
+++ host/webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+const deps = require("./package.json").dependencies;
+
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'host',
+      remotes: {
+        todoMFE: 'todoMFE@http://localhost:3000/remoteEntry.js',
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: false },
+        'react-dom': { singleton: true, eager: true, requiredVersion: false },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
---
+++ todo-mfe/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'todoMFE',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './TodoApp': './src/App.tsx',
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: false },
+        'react-dom': { singleton: true, eager: true, requiredVersion: false },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
